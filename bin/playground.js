386fc896027b2e381ddd1feb4bc3a6288cb329ef
Thomas Richner <thomasrichner+github@gmail.com>
2015-04-11 01:44:07 +0200

    initial commit

---
+++ bin/playground.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+/**
+ * Created by Thomas on 11.04.2015.
+ */
+
+
+var obj = {
+    foo: {
+        bar : {
+            bli : 1
+        }
+    }
+};
+
+var path= 'foo.bar.blub';
+var value = 42;
+
