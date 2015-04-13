02044b6f5400d1b78ef311747ba460a304291df0
Thomas Richner <thomasrichner+github@gmail.com>
2015-04-13 02:50:10 +0200

    gave sessions some more live

--- src/routes/api.js
+++ src/routes/api.js
@@ -82,17 +82,23 @@ router.delete('/waitlist/:id', function(req, res, next) {
 router.post('/verify', function(req, res, next) {
   var key   = req.body.key;
   var vCode = req.body.vCode;
+  var rememberMe = req.body.rememberMe;
   var pilotId = req.eve.char.id;
   if(key && vCode && pilotId){
     service.verifyPilot(key,vCode,pilotId)
         .then(function (isVerified) {
             if(isVerified){
-              req.session.verified = true;
-              req.session.pilotId = pilotId; // implicitly authenticated
-              res.status(204).end();
-              return Q.fulfill();
+                req.session.verified = true;
+                req.session.pilotId = pilotId; // implicitly authenticated
+                var maxAge = 3600000*12; // half day
+                if(rememberMe){
+                    maxAge *= 730; // one year
+                }
+                req.session.cookie.maxAge = maxAge;
+                res.status(204).end();
+                return Q.fulfill();
             }else{
-              return Q.reject();
+                return Q.reject();
             }
         })
         .catch(function (e) {
