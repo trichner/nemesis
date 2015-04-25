0d27dfaee101188343fb97c17e549db9b5dce58b
Thomas Richner <thomasrichner+github@gmail.com>
2015-04-25 16:24:50 +0200

    fixed native methods

--- public/js/EveIGB.js
+++ public/js/EveIGB.js
@@ -3,47 +3,53 @@
  */
 app.factory('EveIGB', function() {
 
+    function notimplemented(){
+        console.log('NOT IMPLEMENTED')
+    }
     // stubs for all methods
     var EveIGB = {
         openEveMail : function(){},
         showInfo : function(){},
-        showPreview : function(){},
-        showRouteTo : function(){},
-        showMap : function(){},
+        showPreview : notimplemented,
+        showRouteTo : notimplemented,
+        showMap :     function(){},
         showFitting : function(){},
-        showContract : function(){},
-        showMarketDetails : function(){},
+        showContract : notimplemented,
+        showMarketDetails : notimplemented,
         requestTrust : function(){},
-        setDestination : function(){},
-        addWaypoint : function(){},
-        joinChannel : function(){},
-        joinMailingList : function(){},
-        createContract : function(){},
-        buyType : function(){},
-        findInContracts : function(){},
-        addToMarketQuickBar : function(){},
-        addContact : function(){},
-        removeContact : function(){},
-        addCorpContact : function(){},
-        removeCorpContact : function(){},
-        block : function(){},
-        addBounty : function(){},
+        setDestination : notimplemented,
+        addWaypoint : notimplemented,
+        joinChannel : notimplemented,
+        joinMailingList : notimplemented,
+        createContract :  notimplemented,
+        buyType : notimplemented,
+        findInContracts : notimplemented,
+        addToMarketQuickBar : notimplemented,
+        addContact : notimplemented,
+        removeContact : notimplemented,
+        addCorpContact : notimplemented,
+        removeCorpContact : notimplemented,
+        block : notimplemented,
+        addBounty : notimplemented,
         inviteToFleet : function(){},
-        startConversation : function(){},
-        showContracts : function(){},
-        showOnMap : function(){},
-        editMember : function(){},
-        awardDecoration : function(){},
-        sendMail : function(){},
-        showContents : function(){},
-        bookmark : function(){}
+        startConversation : notimplemented,
+        showContracts : notimplemented,
+        showOnMap : notimplemented,
+        editMember : notimplemented,
+        awardDecoration : notimplemented,
+        sendMail : notimplemented,
+        showContents : notimplemented,
+        bookmark : notimplemented
     };
-
     // If it is the IGB, attach the real methods
     if(typeof CCPEVE !== 'undefined'){
-        EveIGB.openEveMail = CCPEVE.openEveMail;
-        EveIGB.showInfo = CCPEVE.showInfo;
-        EveIGB.showFitting = CCPEVE.showFitting;
+        // We need a lambda function because CCPEVE functions are native
+        EveIGB.openEveMail = function(){CCPEVE.openEveMail()};
+        EveIGB.showInfo = function(a,b){CCPEVE.showInfo(a,b)};
+        EveIGB.showFitting = function(fit){CCPEVE.showFitting(fit)};
+        EveIGB.requestTrust = function(url){CCPEVE.requestTrust(url)};
+        EveIGB.inviteToFleet = function(arg){CCPEVE.inviteToFleet(arg)};
+
         //for(var m in CCPEVE) {
         //    if(typeof CCPEVE[m] == "function") {
         //        EveIGB[m] = CCPEVE[m];
