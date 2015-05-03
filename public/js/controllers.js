81a70a26ec92227940175e4eebdb280659ed1225
Thomas Richner <thomasrichner+github@gmail.com>
2015-05-03 19:32:51 +0200

    changed wl id storage

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -17,6 +17,7 @@ app.controller('wt-list',[ '$scope','$http','$location','$interval','$window','A
     $scope.waitlistTxt = '';
 
     $scope.getWaitlistId = function () {
+        console.log("ID: " + $location.search())
         return $location.search().waitlistId;
     }
 
